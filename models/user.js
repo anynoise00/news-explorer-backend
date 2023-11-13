6482b3a106591ee0ce22965e940e082ca3d57ee4
anynoise00 <anynoise00@gmail.com>
2023-11-13 13:07:20 -0300

    dev: add user controllers

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const UnauthenticatedError = require('../errors/unauthenticated-error');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -18,8 +19,29 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    default: 'Jacques Costeau',
+    default: 'Elise',
   },
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user)
+        return Promise.reject(
+          new UnauthenticatedError('E-mail ou senha incorretos')
+        );
+      return bcrypt.compare(password, user.password).then((match) => {
+        if (!match)
+          return Promise.reject(
+            new UnauthenticatedError('E-mail ou senha incorretos')
+          );
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
