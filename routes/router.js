1607eabbc0902c97f2b8f12c8fba34303cad2210
anynoise00 <anynoise00@gmail.com>
2023-11-13 13:19:04 -0300

    dev: add login and register routes

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,34 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
+const { createUser, login } = require('../controllers/users');
 
 const router = express.Router();
+
+router.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required(),
+      password: Joi.string().required().min(8),
+      name: Joi.string().min(2).max(30),
+    }),
+  }),
+  createUser
+);
+
+router.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login
+);
+
+// check auth
+
+router.use('/users', require('./users'));
 
 module.exports = router;
